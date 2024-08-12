d9258b481531bb0295297e66a5f68c944feaac96
Karishma Gupta <karishmaneha42@gmail.com>
2024-08-12 23:52:22 +0530

    Navigate Back from ActionBar

--- app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidtrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.androidtrivia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.myNavHostFragment).navigateUp()
     }
 }
