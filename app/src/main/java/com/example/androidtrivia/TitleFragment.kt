c8858543d3898564ac7082e2412e7bda4ed62749
Karishma Gupta <karishmaneha42@gmail.com>
2024-08-23 19:58:41 +0530

    Add Safe Args and pass argument to GameWonFragment

--- app/src/main/java/com/example/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/TitleFragment.kt
@@ -27,7 +27,7 @@ class TitleFragment : Fragment() {
         )
 
         binding.playButton.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+            Navigation.createNavigateOnClickListener(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         )
 
         setHasOptionsMenu(true)
